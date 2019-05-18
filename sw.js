8c7381cadccd7ac3d0615c09662a19c77b40be54
Fziliotti <ffz.ziliotti@gmail.com>
2019-05-18 19:38:54 -0300

    Change manifest and service worker

--- sw.js
+++ sw.js
@@ -1,42 +1,34 @@
-var versao = 9
+var versao = 10;
 
 const filesToCache = [
-  '/BlogFziliotti/',
-  '/BlogFziliotti/cursos/',
-  '/BlogFziliotti/series/',
-  '/BlogFziliotti/about/',
-  '/BlogFziliotti/tags/',
-  '/BlogFziliotti/offline/index.html',
-  '/BlogFziliotti/proporção-Áurea/',
-  '/BlogFziliotti/curso-fast-mba/',
-  '/BlogFziliotti/assets/js/main.js',
+  "/BlogFziliotti/",
+  "/BlogFziliotti/cursos/",
+  "/BlogFziliotti/series/",
+  "/BlogFziliotti/about/",
+  "/BlogFziliotti/tags/",
+  "/BlogFziliotti/offline/index.html",
+  "/BlogFziliotti/assets/js/main.js"
 ];
 
-self.addEventListener("install", function(){
-  console.log("Instalou service worker!")
-})
+self.addEventListener("install", function() {
+  console.log("Instalou service worker!");
+});
 
-self.addEventListener("activate", function(){
-  caches.open("blog-arquivos-" + versao).then(cache => {
-      cache.addAll(filesToCache)
-          .then(function(){
-              caches.delete("blog-arquivos-" + (versao - 1 ))   
-              caches.delete("blog-arquivos")   
-          })
-      
-  })
-})
+self.addEventListener("activate", function() {
+  caches.open("blog-printf-" + versao).then(cache => {
+    cache.addAll(filesToCache).then(function() {
+      caches.delete("blog-printf-" + (versao - 1));
+      caches.delete("blog-printf");
+    });
+  });
+});
 
-
-self.addEventListener("fetch", function(event){
-
-  let pedido = event.request
+self.addEventListener("fetch", function(event) {
+  let pedido = event.request;
   let promiseResposta = caches.match(pedido).then(respostaCache => {
-      let resposta = respostaCache ? respostaCache : fetch(pedido)
-      return resposta
-  })
+    let resposta = respostaCache ? respostaCache : fetch(pedido);
+    return resposta;
+  });
 
-  event.respondWith(promiseResposta)
-
-})
-
+  event.respondWith(promiseResposta);
+});
