47bc53abdabc0c3448ad5a48a184144b20193ed7
Fziliotti <ffz.ziliotti@gmail.com>
2019-05-18 20:33:28 -0300

    change sw

--- sw.js
+++ sw.js
@@ -1,29 +1,28 @@
-var versao = 10;
+var versao = 11;
 
 const filesToCache = [
   "/BlogFziliotti/",
   "/BlogFziliotti/cursos/",
-  "/BlogFziliotti/series/",
   "/BlogFziliotti/about/",
   "/BlogFziliotti/tags/",
   "/BlogFziliotti/offline/index.html",
   "/BlogFziliotti/assets/js/main.js"
 ];
 
-self.addEventListener("install", function() {
+self.addEventListener("install", function () {
   console.log("Instalou service worker!");
 });
 
-self.addEventListener("activate", function() {
+self.addEventListener("activate", function () {
   caches.open("blog-printf-" + versao).then(cache => {
-    cache.addAll(filesToCache).then(function() {
+    cache.addAll(filesToCache).then(function () {
       caches.delete("blog-printf-" + (versao - 1));
       caches.delete("blog-printf");
     });
   });
 });
 
-self.addEventListener("fetch", function(event) {
+self.addEventListener("fetch", function (event) {
   let pedido = event.request;
   let promiseResposta = caches.match(pedido).then(respostaCache => {
     let resposta = respostaCache ? respostaCache : fetch(pedido);
